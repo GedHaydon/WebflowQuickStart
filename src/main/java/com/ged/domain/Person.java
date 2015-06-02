da2cfd083ff6504d8df7b5df4b93fda59acfe2f9
Ged Haydon <ged.haydon@estimators-online.com>
2015-06-02 12:46:38 +0100

    Commiting web flow quick start before changing

---
+++ src/main/java/com/ged/domain/Person.java
@@ -0,0 +1,18 @@
+package com.ged.domain;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class Person {
+
+    /**
+     */
+    private String firstname;
+
+    /**
+     */
+    private String surname;
+}
